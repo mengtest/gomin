9f37cb305341a48b867ea5a544a94fb5f130f9d6
dengjunzhen <dengjunzhen@weshare.com.cn>
2018-12-27 15:30:52 +0800

    delete emeil

--- controllers/default.go
+++ controllers/default.go
@@ -13,9 +13,3 @@ func (c *MainController) Get() {
 	c.Data["Email"] = "astaxie@gmail.com"
 	c.TplName = "index.tpl"
 }
-
-//func Test(c *MainController) {
-//	c.Data["Email"] = "dengjunzhen@weshare.com.cn"
-//	c.Data["Website"] = "dengjunzhen"
-//	c.TplName = "index.tpl"
-//}
